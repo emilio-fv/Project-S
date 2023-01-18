3d065b0b7fc896155f81de918c7599db4c90f41e
emilio-fv <emiliofv.sd@gmail.com>
2023-01-17 22:28:02 -0700

    project setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import { Router, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <p>Hello, world</p>
+      <Router>
+        <Route path='/'/>
+        <Route path='/register' />
+        <Route path='/login' />
+        <Route path='/dashboard' />
+        <Route path='/tickets' />
+        {/* TODO: Add route to project with parameter for project name */}
+        <Route path='/admin' />
+      </Router>
     </div>
   );
 }
