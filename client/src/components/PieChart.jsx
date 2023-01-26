c79d29a9caa87f68df3bd79a0f34baf5fe0f26b3
emilio-fv <emiliofv.sd@gmail.com>
2023-01-26 15:41:14 -0700

    add pie charts to ticket overview

---
+++ client/src/components/PieChart.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const PieChart = (props) => {
+    return (
+        <Box sx={{ maxHeight: '175px', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px' }}>
+            <Pie data={props.data} />
+            <Typography>{props.chartName}</Typography>
+        </Box>
+    )
+}
+export default PieChart ;
+
