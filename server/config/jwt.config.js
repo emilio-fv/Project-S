6007357f6d321ffac66a236bdaf02afb98b94a32
emilio-fv <emiliofv.sd@gmail.com>
2023-02-09 23:23:04 -0700

    JWT setup

---
+++ server/config/jwt.config.js
@@ -0,0 +1,16 @@
+// Import JWT
+const jwt = require('jsonwebtoken');
+
+// Authenticate Function
+const authenticate = (req, res, next) => {
+    jwt.verify(req.cookies.userToken, process.env.SECRET_KEY, (err, payload) => {
+        if (err) {
+            res.status(401).json({ verified: false });
+        } else {
+            next();
+        }
+    })
+};
+
+// Exports
+module.exports = { authenticate: authenticate };
