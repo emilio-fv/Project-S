a50f33288651135799e1f35bc0c2fc81962d4857
emilio-fv <emiliofv.sd@gmail.com>
2023-02-10 13:26:55 -0700

    project model

---
+++ server/models/project.model.js
@@ -0,0 +1,13 @@
+// TODO: Imports Mongoose
+const mongoose = require('mongoose');
+
+// TODO: Project Schema
+const projectSchema = new mongoose.Schema({
+    // TODO: name
+    // TODO: description
+    // TODO: team
+    // TODO: tickets
+}, { timestamps: true });
+
+// TODO: Create Project Model
+// TODO: Exports
