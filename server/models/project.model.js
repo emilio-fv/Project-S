66358f1ccb4ae3c3527566fee5315c168ccbdf56
emilio-fv <emiliofv.sd@gmail.com>
2023-02-12 14:59:26 -0700

    project model

--- server/models/project.model.js
+++ server/models/project.model.js
@@ -1,13 +1,31 @@
-// TODO: Imports Mongoose
+// Imports Mongoose
 const mongoose = require('mongoose');
 
 // TODO: Project Schema
 const projectSchema = new mongoose.Schema({
     // TODO: name
+    name: {
+        type: String,
+        required: [true, "Project name is required."]
+    },
     // TODO: description
+    description: {
+        type: String,
+        required: [true, "Description is required."]
+    },
+    // TODO: projectManager
+    projectManager: {
+        type: String,
+        required: [true, "Project manager is required."]
+    }
     // TODO: team
+        // Array of user ids
     // TODO: tickets
+        // Array of tickets
 }, { timestamps: true });
 
-// TODO: Create Project Model
-// TODO: Exports
+// Create Project Model
+const Project = mongoose.model("Project", projectSchema);
+
+// Exports
+module.exports = { Project: Project }
